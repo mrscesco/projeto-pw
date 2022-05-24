d6fa983f14b83005135026b24877befd878dd665
Camila <camilacescoferreira@gmail.com>
2022-05-24 20:12:52 -0300

    create_migrations

---
+++ pw1805/database/migrations/2022_05_24_223349_create_endereco_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('endereco', function (Blueprint $table) {
+            $table->id();
+            $table->int('cep',8);
+            $table->string('logradouro');
+            $table->string('numero');
+            $table->string('bairro');
+            $table->string('cidade');
+            $table->string('estado');
+            $table->string('complemento');
+
+         /* chave estrangeira */
+
+            $table->unsignedBigInteger('id_cliente');
+            $table->foreign('id_cliente')->references('id')->on('cliente'); 
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('endereco');
+    }
+};
